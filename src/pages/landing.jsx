f6267a0fb95018c29f283938cc24dc919223eaaf
Umutoni-Rita <umurita37@gmail.com>
2023-10-13 14:46:06 +0200

    Sidebar functionality

--- src/pages/landing.jsx
+++ src/pages/landing.jsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function LandingPage() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(prevState => !prevState);
+  };
+
   return (
     <main>
       <nav>
         <img src={logo} alt="valyou Logo" title="Valyou" />
-        <FontAwesomeIcon icon={faBars} className="menu-icon" />
+        <FontAwesomeIcon icon={faBars} className="menu-icon" onClick={toggleSidebar} />
       </nav>
+      {isSidebarVisible && (
+        <div className={`sidebar ${isSidebarVisible ? 'visible' : ''}`}>
+          <Link to='/signup' className="sidebar-link">Signup</Link>
+          <Link to='/login' className="sidebar-link">Login</Link>
+        </div>
+      )}
       <section>
         <div className="green">
           The World's First Blockchain Powered
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
 
 export default function SignupPage() {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(prevState => !prevState);
+  };
   return (
     <main>
       <nav>
         <img src={logo} alt="valyou Logo" title="Valyou" />
-        <FontAwesomeIcon icon={faBars} className="menu-icon" />
+        <FontAwesomeIcon icon={faBars} className="menu-icon" onClick={toggleSidebar}  />
       </nav>
+      {isSidebarVisible && (
+        <div className={`sidebar ${isSidebarVisible ? 'visible' : ''}`}>
+          <Link to='/signup' className="sidebar-link">Signup</Link>
+          <Link to='/login' className="sidebar-link">Login</Link>
+        </div>
+      )}
       <section>
         <div className="hero">
           <h1>Welcome to Valyou X</h1>
